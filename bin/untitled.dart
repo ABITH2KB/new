3bd8420bfb0cbf7596893a03176ba7d8104825cb
ABITH2KB <abith15bathery@gmail.com>
2023-11-28 12:56:30 +0530

    first commit

---
+++ bin/untitled.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+void main() {
+
+
+//   print("enter the num");
+//   int? num= int.parse(stdin.readLineSync()!);
+//   print("the num $num");
+//
+//
+//
+// }
+//
+// void iseven(num) {
+//
+
+print("enter the number");
+int? num= int.parse(stdin.readLineSync()!);
+
+iseven=
+for(int i=0;i<num;i+=2){
+
+
+  print(i);
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
